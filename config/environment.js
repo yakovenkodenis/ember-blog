d166756e4757868dc2ca7e2abb3586530378e18b
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-10-19 11:42:10 +0300

    Add post model

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'blog',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://superblog.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
